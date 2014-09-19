4a5c5b6c61eb7263a45a341ac807dee1ef65b992
31301207 <31301207@089844-P33S10.macklabs.br>
2014-09-19 08:22:57 -0300

    Inicializar a variavel y

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -17,7 +17,7 @@ public class JavaApplication3 {
         // TODO code application logic here
         
        System.out.println("Olá mundo!");
-       int x;
-       
+       int x=0;
+       int y=0;
     }
 }
